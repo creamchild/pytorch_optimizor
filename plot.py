b18c8218deb323f2bd7820711cb43f370c3bcf9b
adam <creamchild72@gmail.com>
2020-12-03 19:46:57 +0900

    2020-12-3

--- plot.py
+++ plot.py
@@ -8,31 +8,39 @@ class dynamicplot:
         self.fig = plt.figure(figsize=(10, 4.8))
         self.ax_loss = self.fig.add_subplot(121)
         self.ax_acc = self.fig.add_subplot(122)
-        self.Loss_list = []
-        self.acc_list = []
+        self.Loss1_list = []
+        self.acc1_list = []
+        self.Loss2_list = []
+        self.acc2_list = []
     
     def plotdefine(self):
         plt.ion()
         self.fig.show()
         self.fig.canvas.draw()
         
-    def showplot(self,loss,acc):
-        self.loss = loss
-        self.acc = acc
+    def showplot(self,loss1,acc1,loss2,acc2):
+        self.loss1 = loss1
+        self.acc1 = acc1
+        self.loss2 = loss2
+        self.acc2 = acc2
         #plot展示
         
         
-        self.Loss_list.append(self.loss)
-        self.acc_list.append(self.acc)
+        self.Loss1_list.append(self.loss1)
+        self.acc1_list.append(self.acc1)
+        self.Loss2_list.append(self.loss2)
+        self.acc2_list.append(self.acc2)
     
         self.ax_loss.clear()
-        self.ax_loss.plot(self.Loss_list)
+        self.ax_loss.plot(self.Loss1_list)
+        self.ax_loss.plot(self.Loss2_list)
         # ax_loss.set_title()
         self.ax_loss.set_xlabel('Iterations')
         self.ax_loss.set_ylabel('Loss')
         
         self.ax_acc.clear()
-        self.ax_acc.plot(self.acc_list)
+        self.ax_acc.plot(self.acc1_list)
+        self.ax_acc.plot(self.acc2_list)
         # ax_acc.set_title()
         self.ax_acc.set_xlabel('Iterations')
         self.ax_acc.set_ylabel('Accuracy')
